188726d0a2d13b258a8b7a54a1b3a783c97b2cfe
Anton Dalsmo <ad@onomondo.com>
2022-09-29 13:55:32 +0200

    now there is stuff

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	fmt.Printf("Starting server at port 8080\n")
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
