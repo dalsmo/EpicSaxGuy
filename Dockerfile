6bbef0eda5e2d2700d4245b81290199f128c6d73
Anton Dalsmo <ad@onomondo.com>
2022-09-30 11:28:07 +0200

    badly dockerized

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+# Build Stage
+# First pull Golang image
+#FROM golang:1.17-alpine as build-env
+FROM golang as build-env
+
+# Set environment variable
+ENV APP_NAME sample-dockerize-app
+ENV CMD_PATH server.go
+
+# Copy application data into image
+COPY . $GOPATH/src/$APP_NAME
+
+WORKDIR $GOPATH/src/$APP_NAME
+
+
+# Budild application
+RUN CGO_ENABLED=0 go build -v -o /$APP_NAME $GOPATH/src/$APP_NAME/$CMD_PATH
+
+# Run Stage
+FROM alpine:3.14
+
+# Set environment variable
+ENV APP_NAME sample-dockerize-app
+
+# Copy only required data into this image
+COPY --from=build-env /$APP_NAME .
+COPY ./data/epic-sax-guy-saxophone.gif /appStuff/data/epic-sax-guy-saxophone.gif
+COPY ./data/epic-sax-guy-saxophone.gif /epic-sax-guy-saxophone.gif
+COPY ./image.html /appStuff/image.html
+
+# Expose application port
+EXPOSE 8081
+
+# Start app
+CMD ./$APP_NAME
--- server.go
+++ server.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func main() {
 
-	fs := http.FileServer(http.Dir("./data"))
+	files, err := ioutil.ReadDir("./appStuff")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
+
+	fs := http.FileServer(http.Dir("./appStuff/data"))
 	http.Handle("/data/", http.StripPrefix("/data/", fs))
 
 	http.HandleFunc("/image", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("attempting to server something")
-		http.ServeFile(w, r, "image.html")
+		http.ServeFile(w, r, "./appStuff/image.html")
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("attempting to server something")
-		http.ServeFile(w, r, "image.html")
+		http.ServeFile(w, r, "./appStuff/image.html")
 	})
 
-	fmt.Println("Server started at port 8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server started at port 8081")
+	http.ListenAndServe(":8081", nil)
 }
